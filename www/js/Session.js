e075bd7f9e307cfb2b2212d0d2c5b5a5f71966a9
Anonymous <dodongobongo@yahoo.com>
2013-10-13 17:13:46 +0200

    new methods for session object

--- www/js/Session.js
+++ www/js/Session.js
@@ -4,16 +4,123 @@ var Session = function() {
 	this.userData = null;
 
 	this.storage = window.localStorage;
-	this.tempStorage = window.sessionStorage;
 
 };
 
-Session.prototype.createUser = function(username, password) {
+// Get storage key for user
+Session.prototype.getUserKey = function(username) {
+	return "user-" + username;
+};
+
+// Get storage key for user trip log
+Session.prototype.getUserLogKey = function(username, logId) {
+	return this.getUserKey() + "-" + logId;
+};
+
+Session.prototype.checkUserData = function(userData) {
+	
+	if(!(userData.logs instanceof Array))
+		userData.logs = [];
 	
 };
 
-Session.prototype.getUserKey = function(username) {
-	return "user-" + username;
+// Add a log object to the local storage
+Session.prototype.addLocalLog = function(log) {
+
+	var sessionLog = {
+		synched: false,
+		data: log.toObject()
+	};
+
+	var logDataJSON = JSON.stringify(sessionLog);
+	var id = Date.now();
+	
+	// Push ID to user's logs
+	this.userData.logs.push(id);
+	
+	// Update local user in storage
+	this.updateLocalUser();
+	
+	this.storage.setItem(
+		this.getUserLogKey(this.userData.username, id),
+		logDataJSON
+	);
+	
+	return id;
+
+};
+
+// Mark a local log as synched
+Session.prototype.setLogSynched = function(id) {
+	
+	var logID = this.getUserLogKey(this.userData.username, id);
+	var logDataJSON = this.storage.getItem(logID);
+	var logData = JSON.parse(logDataJSON);
+	
+	if(logData.synched === true) {
+		console.warn("Session: Synched a log which already was marked as synched.");
+	}
+	
+	logData.synched = true;
+	
+	this.storage.setItem(logID, JSON.stringify(logData));
+	
+
+};
+
+// Get a local trip log
+Session.prototype.getLocalLog = function(id) {
+
+	var logID = this.getUserLogKey(this.userData.username, id);
+	var logDataJSON = this.storage.getItem(logID);
+	var logData = JSON.parse(logDataJSON);
+	
+	return logData;
+
+};
+
+// Remove a trip log from the local storage
+Session.prototype.removeLocalLog = function(id) {
+	
+	var idx = this.userData.logs.indexOf(id);
+	
+	if(idx < 0) {
+		console.warn("Session: Asked to remove invalid log");
+		return;
+	}
+	
+	// Remove from user's log list
+	
+	this.userData.logs.splice(idx, 1);
+	
+	// Update user data in storage
+	
+	this.updateLocalUser();
+	
+	// Remove trip data from storage
+	
+	var logID = this.getUserLogKey(this.userData.username, id);
+	
+	this.storage.removeItem(logID);
+
+};
+
+// Update the storaged object for active local user
+Session.prototype.updateLocalUser = function() {
+	
+	this.storage.setItem(
+		this.getUserKey(username), 
+		JSON.stringify(this.userData)
+	);
+	
+};
+
+Session.prototype.deleteLocalUser = function(username) {
+
+	// Check password?
+
+	this.storage.removeItem(this.getUserKey(username));
+
 };
 
 Session.prototype.createLocalUser = function(username, password) {
@@ -32,7 +139,8 @@ Session.prototype.createLocalUser = function(username, password) {
 
 	var userData = {
 		username: username,
-		password: password
+		password: password,
+		logs: [],
 	};
 	
 	this.storage.setItem(this.getUserKey(username), JSON.stringify(userData));
@@ -49,7 +157,7 @@ Session.prototype.login = function(username, password) {
 
 	var userData = JSON.parse(userDataJSON);
 	
-	//this.validateUserData(userData);
+	this.checkUserData(userData);
 	
 	if(userData.password && userData.password == password) {
 	
