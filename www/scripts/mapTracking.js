3cdfda1efd6b760256e7d4cf1c5d1a050634e461
Kristina Voll <kristina.voll@gmail.com>
2013-10-14 11:56:46 +0200

    added polyline mapTracking.js

--- www/scripts/mapTracking.js
+++ www/scripts/mapTracking.js
@@ -30,6 +30,30 @@ $(document).bind("pageinit", function(event, data){
 		animation : google.maps.Animation.DROP,
 		position : latlng  
 	  });  
+	  
+	  
+	  
+	  var polylineCoordinates = [
+			new google.maps.LatLng(63.430552000000006,10.4198838),
+			new google.maps.LatLng(63.4305551,10.4198696),
+			new google.maps.LatLng(63.430540900000004,10.4198515),
+			new google.maps.LatLng(63.43057169999999,10.4198963),
+			new google.maps.LatLng(63.4305435,10.4198916),
+			new google.maps.LatLng(63.4305541,10.4199129),
+			new google.maps.LatLng(63.430553800000006,10.4198997),
+			new google.maps.LatLng(63.43054599999999,10.4199348),
+			new google.maps.LatLng(63.4305463,10.419937299999999),
+			];
+		
+		var polyline = new google.maps.Polyline({
+			path: polylineCoordinates,
+			strokeColor: '#FF0000',
+			strokeOpacity: 1.0,
+			strokeWeight: 5,
+			editable: true
+		});
+
+  		polyline.setMap(map);   
 	});
 
 });
